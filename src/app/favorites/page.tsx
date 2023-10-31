c19ce6630772687735ec399f077e53d47b775d7c
Kaio-Nguyen <nhatkha06299@gmail.com>
2023-10-31 14:29:13 +0700

    adding in the favorite page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,35 +1,29 @@
-import UpLoadButton from './upload-button';
 import cloudinary from 'cloudinary';
-import { CldImage } from 'next-cloudinary';
-import { CloudinaryImage } from './cloudinary-image';
+import { CloudinaryImage } from '../gallery/cloudinary-image';
+import { SearchResult } from '../gallery/page';
+import { ForceRefresh } from '@/components/force-refresh';
 
 
-export type SearchResult = {
-    public_id: string,
-    tags: string[]
-};
-
-export default async function GalleryPage() {
+export default async function FavoritesPage() {
 
     const results = (await cloudinary.v2.search
-    .expression('resource_type:image')
+    .expression('resource_type:image AND tags=favorite')
     .sort_by('created_at','desc')
     .with_field("tags")
     .max_results(30)
     .execute()) as {resources: SearchResult[]};
 
-    console.log("result", results)
     return (
         <section>
+            <ForceRefresh />
             <div className='flex flex-col gap-8'>
                 <div className="flex justify-between">
-                    <h1 className="text-4xl font-bold">Gallery</h1>        
-                    <UpLoadButton />
+                    <h1 className="text-4xl font-bold">Favorites Image</h1>        
                 </div>
                 <div className='grid grid-cols-4 gap-4'>
                     {results.resources.map((result) =>
                         <CloudinaryImage
-                            path="/gallery"
+                            path='/favorite'
                             key={result.public_id}
                             imageData={result}
                             width="400"
--- src/app/gallery/cloudinary-image.tsx
+++ src/app/gallery/cloudinary-image.tsx
@@ -1,11 +1,42 @@
 "use client";
 
+import { Heart } from "@/components/icons/heart";
+import { FullHeart } from "@/components/icons/full-heart";
 import { CldImage } from "next-cloudinary";
+import { setAsFavoriteAction } from "./actions";
+import { useTransition } from "react";
+import { SearchResult } from "./page"
 
 
+export function CloudinaryImage(props: any & {imageData: SearchResult ; path:string}) {
+    const [transition, startTransition] = useTransition();
 
-export function CloudinaryImage(props: any) {
+    const {imageData} = props;
+    
+    const isFavorited = imageData.tags.includes("favorite");
+
     return (
-        <CldImage {...props}/>
+        <div className="relative">
+            <CldImage {...props}src={imageData.public_id} />
+            {isFavorited ? (
+            <FullHeart 
+                onClick={() => {
+                    startTransition(() => {
+                        setAsFavoriteAction(imageData.public_id, false, props.path);
+                    })
+                }}
+                className="absolute top-2 right-2 hover:text-white text-red-500 cursor-pointer"
+            />
+            ) : (
+            <Heart 
+                onClick={() => {
+                    startTransition(() => {
+                        setAsFavoriteAction(imageData.public_id, true, props.path);
+                    })
+                }}
+                className="absolute top-2 right-2 hover:text-red-500 cursor-pointer"
+            /> )
+            }
+        </div>
     )
 }
